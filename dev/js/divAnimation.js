f8ec56fc9498c918517c048841fff6eb9cca5657
hkapoor2001 <hkapoor2001@gmail.com>
2022-02-09 20:46:43 -0500

    Update _nav.scss

--- dev/js/divAnimation.js
+++ dev/js/divAnimation.js
@@ -1,6 +1,7 @@
 import {gsap} from "gsap";
-gsap.set(".navdiv", {y:-500})
+gsap.set(".navdiv", {scaleY:0})
+// gsap.set(".navdiv",{transformOrigin:bottom,center})
 
 export const divAnimation = new gsap.timeline({paused:true});
 
-divAnimation.to(".navdiv", {y:0, duration:.3, stagger:-.05, ease: "linear"})
+divAnimation.to(".navdiv", {scaleY:-1, duration:.3, stagger:-.05, ease: "linear"})
